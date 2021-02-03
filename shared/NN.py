19aed9e7f711cb47baa37888f0652a1acd8146c0
Kristof Galambos <kris.galambos@gmail.com>
2021-02-03 19:34:51 +0100

    committed to be able to pull

--- shared/NN.py
+++ shared/NN.py
@@ -354,16 +354,17 @@ if __name__ == '__main__':
             # NN.runTuning(3, tuning_mode='random_kt')
 
     else:  # if we are on Kristof's computer
-        # NN = NeuralNetwork(channel='rho_rho', binary=True, write_filename='NN_output', show_graph=False)
-        NN = NeuralNetwork(channel='rho_a1', gen=False, binary=True, write_filename='NN_output', show_graph=False)
+        NN = NeuralNetwork(channel='rho_rho', gen=False, binary=True, write_filename='NN_output', show_graph=False)
+        # NN = NeuralNetwork(channel='rho_a1', gen=False, binary=True, write_filename='NN_output', show_graph=False)
         # NN = NeuralNetwork(channel='a1_a1', binary=True, write_filename='NN_output', show_graph=False)
         # NN.run(2, read=True, from_pickle=True, epochs=10, batch_size=10000)
-        NN.run(1, read=False, from_pickle=False, epochs=10, batch_size=10000)
+        # NN.run(1, read=False, from_pickle=False, epochs=10, batch_size=10000)
+        NN.runTuning(6)
         
-        # for _ in range(7):
-        #     NN.run(1, read=False, from_pickle=False, epochs=10, batch_size=10000)
+        # for i in range(1, 7):
+        #     NN.run(i, read=False, from_pickle=False, epochs=50, batch_size=10000)
         #     print()
         #     print()
-        #     print('ITERATION', _, 'done')
+        #     print('ITERATION', i, 'done')
         #     print()
         #     print()
